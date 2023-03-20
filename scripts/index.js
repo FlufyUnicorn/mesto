8c00932b0ce1f00b6445684720865aaf4cd4f269
Анастасия Зимина <Asya.Zimina777@mail.ru>
2023-03-20 20:51:07 +0300

    refactor oop

--- scripts/index.js
+++ scripts/index.js
@@ -8,23 +8,24 @@ const nameInput = formElementProfile.elements.name
 const jobInput = formElementProfile.elements.job
 const popupProfile = document.querySelector('#popup_profile')
 const popupPlace = document.querySelector('#popup_place')
-const popupImage = document.querySelector('#popup_image')
-const placeInput = formElementPlace.elements.new_place
-const linkInput = formElementPlace.elements.place_link
-const cardTemplate = document.querySelector('#card').content
-const cards = document.querySelector('.cards')
-const closeButtons = document.querySelectorAll('.popup__close-button')
 const popupPicture = document.querySelector('.popup__img')
 const popupCaption = document.querySelector('.popup__img-description')
+const popupImage = document.querySelector('#popup_image')
+const cardTemplate = document.querySelector('#card').content
+const placeInput = formElementPlace.elements.new_place
+const linkInput = formElementPlace.elements.place_link
+const cards = document.querySelector('.cards')
+const closeButtons = document.querySelectorAll('.popup__close-button')
+
 const saveButton = popupPlace.querySelector('.popup__save-button')
 
 nameInput.value = profileName.textContent
 jobInput.value = profileJob.textContent
 
-function closePopup(popup) {
-  popup.classList.remove('popup_opened')
-  document.removeEventListener('keydown', closePopupEsc)
-  popup.removeEventListener('mousedown', closePopupClick)
+function openPopup(popup) {
+  popup.classList.add('popup_opened')
+  document.addEventListener('keydown', closePopupEsc)
+  popup.addEventListener('mousedown', closePopupClick)
 }
 
 function closePopupEsc(evt) {
@@ -40,10 +41,10 @@ function closePopupClick(evt) {
   }
 }
 
-function openPopup(popup) {
-  popup.classList.add('popup_opened')
-  document.addEventListener('keydown', closePopupEsc)
-  popup.addEventListener('mousedown', closePopupClick)
+function closePopup(popup) {
+  popup.classList.remove('popup_opened')
+  document.removeEventListener('keydown', closePopupEsc)
+  popup.removeEventListener('mousedown', closePopupClick)
 }
 
 closeButtons.forEach((button) => {
@@ -72,34 +73,15 @@ function handleFormSubmitProfile(evt) {
 
 formElementProfile.addEventListener('submit', handleFormSubmitProfile)
 
-function addCard(card) {
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true)
-  const cardImage = cardElement.querySelector('.card__image')
-  cardImage.src = card.link
-  cardImage.alt = card.name
-  cardElement.querySelector('.card__name').textContent = card.name
-  cardElement.querySelector('.card__like-button').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('card__like-button_active')
-  })
-  cardElement.querySelector('.card__delete-button').addEventListener('click', function (evt) {
-    evt.target.closest('.card').remove()
-  })
-  cardImage.addEventListener('click', function() {
-    openPopup(popupImage)
-    popupCaption.textContent = card.name
-    popupPicture.src = card.link
-    popupPicture.alt = card.name
-  })
-  return cardElement
-}
-
 function handleFormSubmitPlace(evt) {
   evt.preventDefault()
   const item = {
     name: placeInput.value,
     link: linkInput.value
   }
-  const cardElement = addCard(item)
+
+  const card = new Card(item, '.card')
+  const cardElement = card.generateCard()
   cards.prepend(cardElement)
   closePopup(popupPlace)
   evt.target.reset()
@@ -133,9 +115,53 @@ const initialCards = [
     name: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
-];
+]
 
-initialCards.forEach( (card) => {
-  const cardElement = addCard(card)
+class Card {
+  constructor(data, templateSelector) {
+    this._link = data.link
+    this._name = data.name
+    this._templateSelector = templateSelector
+  }
+  _getTemplate() {
+    return cardTemplate.querySelector(this._templateSelector).cloneNode(true)
+  }
+  generateCard() {
+    this._element = this._getTemplate()
+    this._setEventListeners()
+    this._element.querySelector('.card__image').src = this._link
+    this._element.querySelector('.card__image').alt = this._name
+    this._element.querySelector('.card__name').textContent = this._name
+    return this._element
+  }
+  _setEventListeners() {
+    this._element.querySelector('.card__like-button').addEventListener('click', () => {
+      this._handleCardLike()
+    })
+    this._element.querySelector('.card__delete-button').addEventListener('click', () => {
+      this._handleCardDelete()
+    })
+    this._element.querySelector('.card__image').addEventListener('click', () => {
+      this._handleCardImageClick()
+    })
+  }
+  _handleCardLike() {
+    this._element.querySelector('.card__like-button').classList.toggle('card__like-button_active')
+  }
+  _handleCardDelete() {
+    this._element.closest(this._templateSelector).remove()
+    this._element = null
+  }
+  _handleCardImageClick() {
+    openPopup(popupImage)
+    popupCaption.textContent = this._name
+    popupPicture.src = this._link
+    popupPicture.alt = this._name
+  }
+}
+
+initialCards.forEach( (item) => {
+  const card = new Card(item, '.card')
+  const cardElement = card.generateCard()
   cards.append(cardElement)
 })
--- scripts/validate.js
+++ scripts/validate.js
@@ -7,62 +7,68 @@ const formValidationConfig = {
   saveButtonInactiveClass: 'popup__save-button_inactive',
 }
 
-const showInputError = (formElement, inputElement, errorMessage, config) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
-  inputElement.classList.add(config.inputWithErrorClass)
-  errorElement.textContent = errorMessage
-  errorElement.classList.add(config.inputErrorTextActiveClass)
-}
-
-const hideInputError = (formElement, inputElement, config) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
-  inputElement.classList.remove(config.inputWithErrorClass)
-  errorElement.classList.remove(config.inputErrorTextActiveClass)
-  errorElement.textContent = ''
-
-}
-
-const isValid = (formElement, inputElement, config) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage, config)
-  } else {
-    hideInputError(formElement, inputElement, config)
+class FormValidator {
+  constructor(data, form) {
+    this._data = data
+    this._form = form
   }
-}
-
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid
-  })
-}
-
-const toggleButtonState = (inputList, buttonElement, config) => {
-  if (hasInvalidInput(inputList)) {
-    buttonElement.disabled = true
-    buttonElement.classList.add(config.saveButtonInactiveClass)
-  } else {
-    buttonElement.disabled = false
-    buttonElement.classList.remove(config.saveButtonInactiveClass)
+  enableValidation() {
+      this._setEventListeners(this._form, this._data)
   }
-}
 
-const setEventListeners = (formElement, config) => {
-  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector))
-  const buttonElement = formElement.querySelector(config.saveButtonSelector)
-  toggleButtonState(inputList, buttonElement, config)
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', () => {
-      isValid(formElement, inputElement, config)
-      toggleButtonState(inputList, buttonElement, config)
+  _setEventListeners = (formElement) => {
+    this._inputList = Array.from(formElement.querySelectorAll(this._data.inputSelector))
+    this._buttonElement = formElement.querySelector(this._data.saveButtonSelector)
+    this._toggleButtonState()
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', () => {
+        this._isValid(formElement, inputElement)
+        this._toggleButtonState()
+      })
     })
-  })
+  }
+
+  _toggleButtonState = () => {
+    if (this._hasInvalidInput()) {
+      this._buttonElement.disabled = true
+      this._buttonElement.classList.add(this._data.saveButtonInactiveClass)
+    } else {
+      this._buttonElement.disabled = false
+      this._buttonElement.classList.remove(this._data.saveButtonInactiveClass)
+    }
+  }
+
+  _hasInvalidInput = () => {
+    return this._inputList.some((inputElement) => {
+      return !inputElement.validity.valid
+    })
+  }
+
+  _isValid = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) {
+      this._showInputError(formElement, inputElement, inputElement.validationMessage)
+    } else {
+      this._hideInputError(formElement, inputElement)
+    }
+  }
+
+  _showInputError = (formElement, inputElement, errorMessage) => {
+    this._errorElement = formElement.querySelector(`.${inputElement.id}-error`)
+    inputElement.classList.add(this._data.inputWithErrorClass)
+    this._errorElement.textContent = errorMessage
+    this._errorElement.classList.add(this._data.inputErrorTextActiveClass)
+  }
+
+  _hideInputError = (formElement, inputElement) => {
+    this._errorElement = formElement.querySelector(`.${inputElement.id}-error`)
+    inputElement.classList.remove(this._data.inputWithErrorClass)
+    this._errorElement.classList.remove(this._data.inputErrorTextActiveClass)
+    this._errorElement.textContent = ''
+  }
 }
 
-const enableValidation = (config) => {
-  const formList = Array.from(document.querySelectorAll(config.formSelector))
-  formList.forEach((formElement) => {
-    setEventListeners(formElement, config)
-  })
-}
-
-enableValidation(formValidationConfig)
+const formList = Array.from(document.querySelectorAll('.popup__form'))
+formList.forEach((formElement) => {
+  const validationForm = new FormValidator(formValidationConfig, formElement)
+  validationForm.enableValidation()
+})
